7f1ebd8499c182f2c10bee99239427e8472a610e
Henrique Xaud <xxhenriquexaud@gmail.com>
2021-05-08 06:08:16 -0400

    baic cleaning

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,6 @@ import styles from '../styles/components/Profile.module.css';
 export function Profile() {
 
     const { level } = useContext(ChallengeContext);
-    let ovo, pintinho, galinha, porco = false;
 
     return (
         <div className={styles.profileContainer}>
@@ -15,13 +14,6 @@ export function Profile() {
                 <p>
 
                     <img src="icons/medal.svg" alt="level-1" />
-
-                    {/* <img src="icons/ovo.svg" alt="level-1" /> */}
-
-                    {/* <img src="icons/pintinho.svg" alt="level" />
-                    <img src="icons/galinha.svg" alt="level" />
-                    <img src="icons/porco.svg" alt="level" /> */}
-
 
                     Level {level}
                 </p>
