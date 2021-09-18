d22ff2d2257c4fad2c0c4400f9d590aad902d41a
Henrique Xaud <xxhenriquexaud@gmail.com>
2021-09-18 01:32:43 -0400

    rick evolving added

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ChallengeContext } from '../contexts/ChallengeContext';
 
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
     const { level } = useContext(ChallengeContext);
+    var rick = false;
+
+    if(level > 4){
+        rick = true;
+    }
 
     return (
         <div className={styles.profileContainer}>
+            { !rick ? (
+            <>
             <img src="icons/morty.png" alt="Foto de Perfil" />
             <div>
                 <strong>Morty Smith</strong>
@@ -15,6 +22,54 @@ export function Profile() {
                     Level {level}
                 </p>
             </div>
+            </>
+            ):(
+                <>
+            <img src="icons/rick-colors.png" alt="Foto de Perfil" />
+            <div>
+                <strong>Rick Sanchez</strong>
+                <p>
+                    <img src="/icons/medal.svg" alt="medal"/>
+                    Level {level}
+                </p>
+            </div>
+            </>
+            )
+        }
+            
         </div>
     )
 }
+
+/* { activeChallenge ? (
+    <div className={styles.challengeActive}>
+
+    <header>Ganhe {activeChallenge.xp} xp</header>
+
+    <main>
+        <img src={`icons/${activeChallenge.img}`} alt="objetivo" />
+        <strong>Novo Desafio</strong>
+        <p>{activeChallenge.description}</p>
+    </main>
+
+    <footer>
+        <button
+            type='button'
+            onClick={handleChallengeSucceeded}>Completei
+        </button>
+
+        <button
+            type='button'
+            onClick={handleChallengeFailed}>Desisti
+        </button>
+    </footer>
+</div>
+) : (
+<div className={styles.challengeNotActive}>
+    <p>
+        <img src="icons/flask.png" alt="level" />
+    </p>
+    <strong>Finalize um ciclo para liberar o próximo desafio</strong>
+</div>
+)
+} */
